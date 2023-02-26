ef84eb98ade43b048a3f0bf7d97d2b3311b0ff88
Miroslav Ristic <risticmiroslav@gmail.com>
2023-02-26 17:16:34 +0100

    Update monitor_and_ocr.sh

--- monitor_and_ocr.sh
+++ monitor_and_ocr.sh
@@ -2,5 +2,5 @@
 input_file=$1
 output_file=$(basename inputfile)_searchable.pdf
 echo 'ocr-ing $input_file to $outputfile'
-/home/bhg/workspace/ocr/.venv/bin/ocrmypdf --rotate-pages --rotate-pages-threshold 1 --redo-ocr --ocr-engine gcv --output-type pdfa -l hrv $input_file $output_file
+ocrmypdf --rotate-pages --rotate-pages-threshold 1 --redo-ocr --ocr-engine gcv --output-type pdfa -l hrv $input_file $output_file
 
