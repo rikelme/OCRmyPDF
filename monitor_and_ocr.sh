09243327dd36c31f68220ebfb522a66fb763a16e
Miroslav Ristic <risticmiroslav@gmail.com>
2023-02-26 17:17:02 +0100

    Update monitor_and_ocr.sh

--- monitor_and_ocr.sh
+++ monitor_and_ocr.sh
@@ -2,5 +2,5 @@
 input_file=$1
 output_file=$(basename inputfile)_searchable.pdf
 echo 'ocr-ing $input_file to $outputfile'
-ocrmypdf --rotate-pages --rotate-pages-threshold 1 --redo-ocr --ocr-engine gcv --output-type pdfa -l hrv $input_file $output_file
+ocrmypdf --rotate-pages --rotate-pages-threshold 1 --redo-ocr --ocr-engine gcv --output-type pdfa -l eng,hrv,srp $input_file $output_file
 
